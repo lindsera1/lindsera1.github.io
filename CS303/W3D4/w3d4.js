a59e2195d94f5622dd2291fcc3af7ac5bd0ab668
lindsera1 <52899223+lindsera1@users.noreply.github.com>
2020-10-08 16:23:50 -0500

    Update w3d4.js

--- CS303/W3D4/w3d4.js
+++ CS303/W3D4/w3d4.js
@@ -38,3 +38,61 @@ function askPassword(ok, fail) {
   };
   
   askPassword(user1.login.bind(user1,true), user1.login.bind(user1, false)); // ?
+
+  //Exam2 Redo Questions
+
+  //Arrow functions are best characterized as function expressions, 
+  //not declarations
+
+  //Consider the following code. What appears?
+
+  "use strict"
+  function perimeter(){
+    console.log(this);
+    return 4 * this.side;
+  }
+  const shape = {side: 5, perimeter: perimeter};
+  shape.perimeter()
+
+  //The Shape object
+
+  //Recall the makeCounter function. Write a similar function with a 
+  //closure, makeAccount() to keep track of an account balance
+
+  function makeAccount(){
+    let balance = 0;
+    return function(transation,amount){
+      if(transation === "add"){
+          balance += amount;
+          return balance;
+      }
+      if(transaction === "debit"){
+          balance -= amount;
+          return balance;
+      }
+    }
+  }
+
+  let account1 = makeAccount();
+  console.log(account1("add",1000));
+
+  /*Write a function tenClock() that will show the time for 10 seconds. 
+  We will want this 10 second timer to display on a web page eventually, 
+  but for now log the current time to the console every second. It 
+  should show the current time in hh:mm:ss format. Recall the Date 
+  object and methods getHours,getMinutes, getSeconds.*/
+
+  function tenClock(){
+    function now(){
+      let now = new Date();
+      console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+    }
+    let timer = setInterval(now, 1000);
+    function stop_Interval(){
+      clearInterval(timer);
+    }
+    setTimeout(stop_Interval, 12000);
+    console.log(timer);
+  }
+
+  tenClock();
